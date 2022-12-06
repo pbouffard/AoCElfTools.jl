ed38a339bd6a58ada843db5dae96a9b5e3c258a6
Jack Shannon <j.b.shannon@lse.ac.uk>
2022-12-06 19:06:03 +0000

    added Day 5 tests

---
+++ test/2022/05.jl
@@ -0,0 +1,24 @@
+i, istr = check_day(@__FILE__)
+
+@testset "Day $istr" begin
+
+    # Puzzle answers
+    answers = Dict(
+        "sample" => ("CMZ", "MCD"),
+        "jbshannon" => ("QMBMJDFTD", "NBTVTJNFJ"),
+    )
+
+    # Test parsing the sample input
+    targetpath = joinpath(datadir, "sample", "$istr.txt")
+    stacks = [['Z', 'N'], ['M', 'C', 'D'], ['P']]
+    instructions = [[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]]
+    target = (stacks, instructions)
+    sample = open(parse_input(i), targetpath)
+    @test sample == target
+
+    # Test solutions
+    for (dir, answer) in answers
+        input_path = joinpath(datadir, dir, "$istr.txt")
+        @test open(parse_solve(i), input_path) == answer
+    end
+end
