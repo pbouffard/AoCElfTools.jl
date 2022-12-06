7dd3783188d8edf52a37e09abbd4ed5a454a1984
Jack Shannon <j.b.shannon@lse.ac.uk>
2022-12-06 16:50:43 +0000

    added `userpath` utility function

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,5 @@
 """
-    samplepath(i) -> String
+    samplepath(i::Int) -> String
 
 Return the path to the sample input file for Day `i`.
 
@@ -12,7 +12,27 @@ julia> samplepath(14)
 "test/data/sample/14.txt"
 ```
 """
-function samplepath(i)
+function samplepath(i::Int)
     istr = @sprintf("%02d", i)
     return joinpath("test", "data", "sample", "$istr.txt")
+end
+
+
+"""
+    userpath(user, i) -> String
+
+Return the path to `user`'s input file for Day `i`.
+
+# Examples
+```jldoctest
+julia> userpath("jbshannon", 1)
+"test/data/jbshannon/01.txt"
+
+julia> userpath("jbshannon", 13)
+"test/data/jbshannon/13.txt"
+```
+"""
+function userpath(user, i::Int)
+    istr = @sprintf("%02d", i)
+    return joinpath("test", "data", user, "$istr.txt")
 end
