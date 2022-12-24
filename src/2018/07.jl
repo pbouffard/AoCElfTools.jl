11533574e0047b19422d4b74547894c1567e657b
Patrick Bouffard <bouffard@eecs.berkeley.edu>
2022-12-24 05:26:51 -0800

    day 7 part 1

--- src/2018/07.jl
+++ src/2018/07.jl
@@ -3,100 +3,106 @@ module Day7
 import ...parseday
 import ...solveday
 
-"""
-Day 6: The Sum of Its Parts
-"""
+using MetaGraphs
+using Graphs
+using GraphPlot
 
-function day7a(io)
-  result = ""
-  for L ∈ readlines(io)
-    @info "------------------"
-    pred, succ = string.([L[6], L[37]])
-    @info pred, succ
-    ipredrng = findfirst(pred, result)
-    isuccrng = findfirst(succ, result)
-    @info ipredrng == nothing ? "nothing" : ipredrng
-    @info isuccrng == nothing ? "nothing" : isuccrng
-
-    if ipredrng == nothing
-      if isuccrng == nothing
-        @info "case 1"
-        # neither occur in result, put them at the head
-        result = pred * succ * result
-      else
-        @info "case 2"
-        # succ is already there but not pred, put pred before succ
-        isucc = isuccrng[1]
-        result = result[1:isucc-1] * pred * result[isucc:end]
-      end
-    else
-      if isuccrng == nothing
-        @info "case 3"
-        # pred there, succ is not, insert succ after pred
-        ipred = ipredrng[1]
-        # .. in the first spot where it is alphabetically greater
-        @info "Looking for a place for $(succ) in $(result[ipred+1:end])"
-        @info "ipred = $(ipred)"
-        isuccins = findfirst(x -> x > succ[1], result[ipred+1:end])
-        @show isuccins
-        if isuccins == nothing
-          result = result * succ
-        else
-          inspoint = ipred + isuccins[1] - 1
-          result = result[1:inspoint] * succ * result[inspoint+1:end]
-        end
-      else
-        @info "case 4"
-        # both pred and succ are there, move succ after pred if it isn't already
-        ipred = ipredrng[1]
-        isucc = isuccrng[1]
-        @info ipred, isucc
-        if isucc < ipred
-          @info "Looking for a place for $(succ) in $(result[ipred+1:end])"
-          isuccins = findfirst(x -> x > succ[1], result[ipred+1:end])
-          if isuccins == nothing
-            @info "Case 4a"
-            result = result[1:isucc-1] * result[isucc+1:end] * succ
-          else
-            @info "Case 4b"
-            result = result[1:isucc-1] * result[isucc+1:end]
-            ipred -= 1
-            inspoint = ipred + isuccins[1] - 1
-            result = result[1:inspoint] * succ * result[inspoint+1:end]
-          end
-        elseif isucc == ipred
-          @info "Case 4c"
-          # make sure they are in alpha order
-          loc1, loc2 = minmax(succ, pred)
-          result = result[i:isucc-1] * loc1 * loc2 * result[isucc+2:end]
-        else # isucc > ipred
-          @info "Case 4d"
-          # do nothing - already in order
-        end
-      end
-    end
-    @info result
-  end
-  return result
+"Remove any vertices with no incoming or outgoing edges"
+function cull_graph(g)
+  used_verts = filter(i -> indegree(g, i) != 0 || outdegree(g, i) != 0, 1:nv(g))
+  g2 = induced_subgraph(g, used_verts)[1]
+  set_indexing_prop!(g2, :name) # why isn't this preserved?
+  @debug g2.indices
+  return g2
 end
 
-# function parseday(::Val{7}, ::Val{2018})
-#   function p(io)
-#     # Read the inputs
-#     P = Dict{Int,Tuple{Int,Int}}()
-#     for (i, L) ∈ enumerate(readlines(io))
-#       (c, r) = Tuple(parse.(Int, split(L, ",")))
-#       P[i] = (r + 1, c + 1)
-#       @debug P[i]
-#     end
-#     return P
-#   end
-# end
 
+"Since the vertices remain in alphabetical order whichever is the first with no incoming edges is the next step to be performed"
+function next_ready(g)
+  for step in 'A':'Z'
+    try
+      v = g[step, :name]
+      @debug step, v, indegree(g, v)
+      if indegree(g, v) == 0
+        return v
+      end
+    catch e
+      # handle case if vertex wasn't found
+      @debug "caught on $step"
+    end
+  end
+  # There now should be only 1 vertex remaining:
+  @assert nv(g) == 1
+  return 1
+end
+
+
+function step!(g)
+  step = next_ready(g)
+  vname = get_prop(g, step, :name)
+  rem_vertex!(g, step)
+  return vname
+end
+
+function process!(g)
+  steps = []
+  while nv(g) > 0
+    @debug steps
+    try
+      step = step!(g)
+      push!(steps, step)
+      @info steps
+    catch e
+      @info e
+      break
+    end
+  end
+  @show g
+  return join(steps, "")
+end
+
+function dependants(g, step_name)
+
+end
+
+vertex_names(g) = [props(g, i)[:name] for i in 1:nv(g)]
+
+
+"Use GraphPlot to display the digraph with labeled vertices"
+function gplot(g)
+  labels = vertex_names(g)
+  GraphPlot.gplot(g; nodelabel=labels)
+end
+
+"Return parsing function for Day 7"
+function parseday(::Val{7}, ::Val{2018})
+  "Construct a MetaDiGraph from the provided input"
+  function p(io)
+    g = MetaDiGraph()
+    for c in 'A':'Z'
+      add_vertex!(g, :name, c)
+    end
+    set_indexing_prop!(g, :name)
+
+    pattern = r"Step ([A-Z]) must be finished before step ([A-Z]) can begin."
+    for line in eachline(io)
+      m = match(pattern, line)
+      sstr, dstr = m.captures
+      si, di = (s -> Char(only(s)) - 'A' + 1).(m.captures)
+      add_edge!(g, (si, di))
+    end
+
+    g = cull_graph(g)
+    @show g
+    return g
+  end
+end
 
 function solveday(::Val{7}, ::Val{2018})
   function f(io)
-    part1 = day7a(io)
+    g = parseday(Val(7), Val(2018))(io)
+    g2 = deepcopy(g)
+    part1 = process!(g)
     part2 = nothing
     return (part1, part2)
   end
