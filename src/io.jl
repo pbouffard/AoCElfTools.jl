7795da9fa17d5f11dfceea3b775b9d369b152dc1
Patrick Bouffard <bouffard@eecs.berkeley.edu>
2022-12-28 10:42:34 -0800

    eachint

--- src/io.jl
+++ src/io.jl
@@ -1,4 +1,6 @@
 
+using StringViews
+
 struct EachByte{IOT<:IO}
   stream::IOT
   ondone::Function
@@ -53,3 +55,81 @@ Base.IteratorSize(::Type{<:EachByte}) = Base.SizeUnknown()
 isdone(itr::EachByte, state...) = eof(itr.stream)
 
 
+struct EachInt{IOT<:IO}
+  stream::IOT
+  ondone::Function
+  EachInt(stream::IO=stdin; ondone::Function=()->nothing) = new{typeof(stream)}(stream, ondone)
+end
+
+"""
+    eachint(io::IO=stdin)
+    eachint(filename::AbstractString)
+
+Create an iterable `eachint` object that will yield integers from an I/O stream or a file containing
+space-separated integers. Iteration calls [`read`](@ref) on the stream argument repeatedly. When
+called with a file name, the file is opened once at the beginning of iteration and closed at the
+end. If iteration is interrupted, the file will be closed when the `EachInt` object is garbage
+collected.
+
+# Examples
+```jldoctest
+julia> open("my_file.txt", "w") do io
+  write(io, "0 1 2 3 4");
+end;
+
+julia> for int in eachint("my_file.txt")
+@show int
+end
+int = 0
+int = 1
+int = 2
+int = 3
+int = 4
+
+julia> rm("my_file.txt");
+````
+"""
+function eachint(stream::IO=stdin)
+  EachInt(stream)::EachInt
+end
+
+function eachint(filename::AbstractString)
+  bs = open(filename)
+  EachInt(bs, ondone=()->close(bs))::EachInt
+end
+
+const _whitespace = ['\n', '\r', '\t', ' '] .|> UInt8
+function Base.iterate(itr::EachInt, state=nothing)
+  function intresult()
+    sv = StringView(intchars)
+    (parse(Int, sv), nothing)
+  end
+
+  intchars = Vector{UInt8}()
+  while true
+    if eof(itr.stream)
+      if !isempty(intchars)
+        return intresult()
+      else
+        return (itr.ondone(); nothing)
+      end
+    end
+
+    c = read(itr.stream, UInt8)::UInt8
+    if c in _whitespace
+      if !isempty(intchars)
+        return intresult()
+      else
+        continue
+      end
+    end
+    
+    push!(intchars, c)
+  end
+end
+
+eltype(::Type{<:EachInt}) = Int
+
+Base.IteratorSize(::Type{<:EachInt}) = Base.SizeUnknown()
+
+isdone(itr::EachInt, state...) = eof(itr.stream)
---
+++ test/puzzles.jl
@@ -0,0 +1,13 @@
+"""
+Helper function, expects a string input of the form "/path/to/YYYY/DD.jl" and returns a tuple of the day (DD) as an integer, the day as a string, and the year as an integer.
+"""
+function check_day(filepath)
+  istr = filepath |> basename |> splitext |> first
+  year = filepath |> splitdir |> first |> splitdir |> last
+  return parse(Int, istr), istr, parse(Int, year)
+end
+
+@testset verbose = true for year in yearsavailable()
+    include(joinpath(@__DIR__, string(year), string(year)) * ".jl")
+  end
+end
